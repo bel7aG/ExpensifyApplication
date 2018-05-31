f38fa23baa5f6411275596d13dc2c43d5aeb4eba
Belhassen <Belhassen.Gharsallah@isetrades.com>
2018-05-31 02:16:20 +0200

    Header Component 11%

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <h1>Expensify Application</h1>
+        <Header />
       </div>
     );
   }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => (
+  <header className="header">
+    <h1 className="heading-primary">
+      <span className="heading-primary-main">
+        bel7aG
+      </span>
+      <span className="heading-primary-sub">
+        Expensify Application
+      </span>
+    </h1>
+  </header>
+);
+
+export default Header;
